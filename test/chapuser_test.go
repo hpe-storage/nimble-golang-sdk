4fd5a523112eb0f20d0eb371a6d44bf1f6e49f41
Kousalya Nachimuthu <kousalya.nachimuthu@hpe.com>
2020-12-01 05:13:13 -0800

    Adding tests for chapuser, snapshot
    Signed-off-by: Kousalya Nachimuthu <kousalya.nachimuthu@hpe.com>

---
+++ test/chapuser_test.go
@@ -0,0 +1,114 @@
+/*
+Chap User tests
+---------------
+  1. Create a Chap user
+  2. Update Chap user name, description
+  3. Create Initiator group & Update Chap user iqns
+  4. Delete Chap user when iqn is associated
+      It should fail, validate error message
+  5. Remove iqns from chap user & delete chap user
+  6. Delete initiator group
+Note: Password change is not supported through ansible module
+Note: Step 3, 4, 5 application for versions greater than fiji only
+*/
+
+package test
+
+import (
+	"testing"
+
+	"github.com/hpe-storage/nimble-golang-sdk/pkg/client/v1/nimbleos"
+	"github.com/hpe-storage/nimble-golang-sdk/pkg/param"
+	"github.com/hpe-storage/nimble-golang-sdk/pkg/service"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+const chapUserName = "testchapuser"
+
+type ChapuserWorkflowSuite struct {
+	suite.Suite
+	groupService        *service.NsGroupService
+	userService         *service.UserService
+	chapuserService     *service.ChapUserService
+	initiatorGrpService *service.InitiatorGroupService
+}
+
+func (suite *ChapuserWorkflowSuite) SetupSuite() {
+	groupService, err := config()
+	assert.Nilf(suite.T(), err, "Could not connect to array")
+	suite.groupService = groupService
+	suite.chapuserService = groupService.GetChapUserService()
+	suite.initiatorGrpService = groupService.GetInitiatorGroupService()
+}
+
+func (suite *ChapuserWorkflowSuite) CreateIGIscsiInitiators(iqnLable string, iqn string, ipAddress string) string {
+	iscsiInitiator := &nimbleos.NsISCSIInitiator{
+		Label:     param.NewString(iqnLable),
+		Iqn:       param.NewString(iqn),
+		IpAddress: param.NewString(ipAddress),
+	}
+	var initiatorList []*nimbleos.NsISCSIInitiator
+	initiatorList = append(initiatorList, iscsiInitiator)
+	newIG := &nimbleos.InitiatorGroup{
+		Name:            param.NewString("TestChapIGIscsi"),
+		Description:     param.NewString("Workflow initiator group"),
+		AccessProtocol:  nimbleos.NsAccessProtocolIscsi,
+		IscsiInitiators: initiatorList,
+	}
+	igResp, err := suite.initiatorGrpService.CreateInitiatorGroup(newIG)
+	assert.Nilf(suite.T(), err, "Unable to create initiator group, err: %v", err)
+	return *igResp.ID
+}
+
+func (suite *ChapuserWorkflowSuite) TestCreateModifyDeleteChapuser() {
+	chapUserPassword := "password_25-24"
+	chapUserescription := "Chap User created by SDK module for testing"
+	// Create chapuser without initiator
+	newChapUser := &nimbleos.ChapUser{
+		Name:        param.NewString(chapUserName),
+		Password:    param.NewString(chapUserPassword),
+		Description: param.NewString(chapUserescription),
+	}
+	_, err := suite.chapuserService.CreateChapUser(newChapUser)
+	assert.Nilf(suite.T(), err, "Chap User creation failed")
+	chapuserResp, err := suite.chapuserService.GetChapUserByName(chapUserName)
+	assert.Nilf(suite.T(), err, "Failed to get chap user by name")
+	assert.Equal(suite.T(), chapUserescription, *chapuserResp.Description, "In-correct Description for Chap user")
+	chapuserID := *chapuserResp.ID
+
+	// Update chapuser with initiators
+	iqnLable := "chapiqn1"
+	iqn := "iqn.1994-05.com.redhat:48e040d53bf6"
+	ipAddress := "22.2.2.2"
+	igID := suite.CreateIGIscsiInitiators(iqnLable, iqn, ipAddress)
+	iscsiInitiator := &nimbleos.NsISCSIIQN{
+		Name: param.NewString(iqn),
+	}
+	var initiatorList []*nimbleos.NsISCSIIQN
+	initiatorList = append(initiatorList, iscsiInitiator)
+	updateChapUser := &nimbleos.ChapUser{
+		InitiatorIqns: initiatorList,
+	}
+	_, err = suite.chapuserService.UpdateChapUser(chapuserID, updateChapUser)
+	assert.Nilf(suite.T(), err, "Chap User creation failed")
+	_, err = suite.chapuserService.GetChapUserByName(chapUserName)
+	assert.Nilf(suite.T(), err, "Failed to get chap user by name")
+
+	// Delete Chap user with initiators associated
+	err = suite.chapuserService.DeleteChapUser(chapuserID)
+	assert.NotNilf(suite.T(), err, "Failed to delete chap user")
+
+	// Delete Initiatorgroup
+	err = suite.initiatorGrpService.DeleteInitiatorGroup(igID)
+	assert.Nilf(suite.T(), err, "Failed to delete initiator group")
+
+	// Delete Chap user
+	err = suite.chapuserService.DeleteChapUser(chapuserID)
+	assert.Nilf(suite.T(), err, "Failed to delete chap user")
+
+}
+
+func TestChapuserWorkflowSuite(t *testing.T) {
+	suite.Run(t, new(ChapuserWorkflowSuite))
+}
---
+++ test/snapshot_test.go
@@ -0,0 +1,161 @@
+/*
+Create/Modify/Delete Snapshot tests:
+-----------------
+ 1. Create a volume
+ 2. Creat an online snapshot snapshot1 for the volume
+      Validate if Snapshot is created for the volume
+ 3. Creat an offline snapshot snapshot2 for the volume
+ 4. Delete volume with online snapshot
+			It should fail
+ 5. Update following params for the same snapshot
+    		online, description, name
+         writable value cannot be modified
+ 6. Create clone volume with snapshot1
+ 7. Delete snapshot1 which has cloned volume
+         It should fail
+ 8. Delete the cloned volume
+ 9. Delete online snapshot for the volume
+         - API allows to delete online volume
+ 10. Delete offline snapshot from the volume
+ 11. Delete the volume
+*/
+
+package test
+
+import (
+	"testing"
+
+	"github.com/hpe-storage/nimble-golang-sdk/pkg/client/v1/nimbleos"
+	"github.com/hpe-storage/nimble-golang-sdk/pkg/param"
+	"github.com/hpe-storage/nimble-golang-sdk/pkg/service"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type SnapshotWorkflowSuite struct {
+	suite.Suite
+	groupService    *service.NsGroupService
+	snapshotService *service.SnapshotService
+	volumeService   *service.VolumeService
+}
+
+func (suite *SnapshotWorkflowSuite) SetupSuite() {
+	groupService, err := config()
+	if err != nil {
+		suite.T().Errorf("Unable to connect to group: %v", err.Error())
+	}
+	suite.groupService = groupService
+	suite.snapshotService = groupService.GetSnapshotService()
+	suite.volumeService = groupService.GetVolumeService()
+}
+
+// Create a volume/clone volume
+func (suite *SnapshotWorkflowSuite) createVolume(volumeName string, baseSnapshotID string) string {
+
+	var sizeField int64 = 5120
+	newVolume := &nimbleos.Volume{
+		Name:   param.NewString(volumeName),
+		Size:   &sizeField,
+		Online: param.NewBool(false),
+	}
+	if baseSnapshotID != "" {
+		newVolume.BaseSnapId = param.NewString(baseSnapshotID)
+		newVolume.Clone = param.NewBool(true)
+	}
+	volResp, err := suite.volumeService.CreateVolume(newVolume)
+	assert.Nilf(suite.T(), err, "Unable to create volume %v", volumeName)
+	return *volResp.ID
+}
+
+func (suite *SnapshotWorkflowSuite) deleteVolume(volumeID string) error {
+	delVol := suite.volumeService.DeleteVolume(volumeID)
+	return delVol
+}
+
+// Creat an offline/online snapshot for the volume. Delete volume
+func (suite *SnapshotWorkflowSuite) TestCreateUpdateSnapshot() {
+	// Create, Get, Delete Offline Snapshot
+	volumeName := "testvolumesnap1"
+	volumeID := suite.createVolume(volumeName, "")
+	offlineSnapshotName := "snapoffline"
+	newOfflineSnapshot := &nimbleos.Snapshot{
+		Name:  param.NewString(offlineSnapshotName),
+		VolId: param.NewString(volumeID),
+	}
+	_, err := suite.snapshotService.CreateSnapshot(newOfflineSnapshot)
+	assert.Nilf(suite.T(), err, "Failed to create snapshot")
+
+	getSnapResp, err := suite.snapshotService.GetSnapshotByName(volumeName)
+	assert.Nilf(suite.T(), err, "Failed to get snapshot")
+
+	err = suite.snapshotService.DeleteSnapshot(*getSnapResp[0].ID)
+	assert.Nilf(suite.T(), err, "Failed to delete snapshot")
+
+	// Create Online Snapshot
+	onlineSnapshotName := "snaponline"
+	newOnlineSnapshot := &nimbleos.Snapshot{
+		Name:     param.NewString(onlineSnapshotName),
+		VolId:    param.NewString(volumeID),
+		Online:   param.NewBool(true),
+		Writable: param.NewBool(true),
+	}
+	snapResp, err := suite.snapshotService.CreateSnapshot(newOnlineSnapshot)
+	assert.Nilf(suite.T(), err, "Failed to create snapshot")
+	onlineSnapshotID := *snapResp.ID
+
+	// Delete Volume with online snapshot
+	err = suite.deleteVolume(volumeID)
+	assert.NotNilf(suite.T(), err, "Volume deletion expected to throw error, as there are online snapshots")
+
+	// Offline the Snapshot & update description
+	updateSnapshot := &nimbleos.Snapshot{
+		Name:        param.NewString("snapshot2"),
+		Online:      param.NewBool(false),
+		Description: param.NewString("Offline the snapshot"),
+	}
+	_, err = suite.snapshotService.UpdateSnapshot(onlineSnapshotID, updateSnapshot)
+	assert.Nilf(suite.T(), err, "Failed to update snapshot")
+
+	err = suite.deleteVolume(volumeID)
+	assert.Nilf(suite.T(), err, "Failed to delete volume")
+}
+
+// Snapshot Clone Volume test
+func (suite *SnapshotWorkflowSuite) TestCloneVolumeSnapshot() {
+	volumeName := "testvolumesnap2"
+	parentVolumeID := suite.createVolume(volumeName, "")
+	baseSnapshotName := "snapoffline"
+	newOfflineSnapshot := &nimbleos.Snapshot{
+		Name:   param.NewString(baseSnapshotName),
+		VolId:  param.NewString(parentVolumeID),
+		Online: param.NewBool(true),
+	}
+	snapResp, err := suite.snapshotService.CreateSnapshot(newOfflineSnapshot)
+	assert.Nilf(suite.T(), err, "Failed to create snapshot")
+	baseSnapshotID := *snapResp.ID
+
+	// Create clone volume
+	cloneVolumeName := "clonevolumesnap2"
+	cloneVolumeID := suite.createVolume(cloneVolumeName, baseSnapshotID)
+
+	// Delete Snapshot with Clone Volume
+	err = suite.snapshotService.DeleteSnapshot(baseSnapshotID)
+	assert.NotNilf(suite.T(), err, "Snapshot deletion should have thrown error")
+
+	// Delete clone volume
+	err = suite.deleteVolume(cloneVolumeID)
+	assert.Nilf(suite.T(), err, "Failed to delete volume")
+
+	// Delete Snapshot after deleting Clone Volume
+	err = suite.snapshotService.DeleteSnapshot(baseSnapshotID)
+	assert.Nilf(suite.T(), err, "Failed to delete snapshot")
+
+	// Delete parent volume
+	err = suite.deleteVolume(parentVolumeID)
+	assert.Nilf(suite.T(), err, "Failed to delete volume")
+
+}
+
+func TestSnapshotWorkflowSuite(t *testing.T) {
+	suite.Run(t, new(SnapshotWorkflowSuite))
+}
