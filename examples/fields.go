314ebdfd669c68d7f1afa291c10aa264ebe97d47
irenna-lumbuun <irenna.lumbuun@hpe.com>
2021-08-04 15:20:05 -0700

    Get rid of environment variables
    Signed-off-by: irenna-lumbuun <irenna.lumbuun@hpe.com>

--- examples/fields.go
+++ examples/fields.go
@@ -3,130 +3,32 @@ package main
 
 import (
 	"fmt"
-	"flag"
 	"os"
-	"strings"
 	"github.com/hpe-storage/nimble-golang-sdk/pkg/client/v1/nimbleos"
 	"github.com/hpe-storage/nimble-golang-sdk/pkg/param"
 	"github.com/hpe-storage/nimble-golang-sdk/pkg/service"
 )
 
-func handleOptionError(target string){
-	fmt.Printf("ERROR: Environment variable SDK_TARGET_%s is not defined.\n", strings.ToUpper(target))
-	os.Exit(1)
-}
-
-/*
-	This function returns: host, isTenant (indicates whether we're using regular user or tenantUser), username, and password
-*/
-func parseOptionalArgument()(string, bool, string, string){
-	// define flags
-	host := flag.String("host", "", "SDK Target Host")
-	user := flag.String("user", "", "SDK Target User")
-	password := flag.String("user-password", "", "SDK Target User Password")
-	tenant := flag.String("tenant", "", "SDK Target Tenant")
-	tenantPassword := flag.String("tenant-password", "", "SDK Target Tenant Password")
-
-	flag.Parse()
-	fmt.Println(*host, *user, *password, *tenant, *tenantPassword);
-
-	if *host == "" {
-		*host = os.Getenv("SDK_TARGET_HOST")
-	}
-
-	if *user == "" {
-		*user = os.Getenv("SDK_TARGET_USER")
-	}
-
-	if *password == "" {
-		*password = os.Getenv("SDK_TARGET_USER_PASSWORD")
-	}
-
-	if *tenant == "" {
-		*tenant = os.Getenv("SDK_TARGET_TENANT_USER")
-	}
-
-	if *tenantPassword == "" {
-		*tenantPassword = os.Getenv("SDK_TARGET_TENANT_PASSWORD")
-	}
-
-	// Host has to be defined
-	if *host == "" {
-		handleOptionError("host")
-	}
-
-	// Either (user & password) or (tenant & tenantPassword) has to be defined
-	isTenant := false
-
-	if *user == "" && *password== "" && *tenant == "" && *tenantPassword == "" {
-		fmt.Println("ERROR: Must define either a user or a tenant.")
-		os.Exit(1)
-	}
-
-	// Initalize with user instead of tenant
-	if *tenant == "" && *tenantPassword == "" {
-		// Error handling
-		if (*user != "" || *password != "") {
-			if *user != "" && *password == "" {
-				handleOptionError("user_password")
-			}
-			if *user == "" && *password != "" {
-				handleOptionError("user")
-			}
-		}
-		return *host, isTenant, *user, *password
-	}
-
-	// Initialize with a tenant
-	if *user == "" && *password == "" {
-		isTenant = true
-		// Error handling
-		if (*tenant != "" || *tenantPassword != ""){
-			if *tenant != "" && *tenantPassword == "" {
-				handleOptionError("tenant_password")
-			}
-			if *tenant == "" && *tenantPassword != "" {
-				handleOptionError("tenant_user")
-			}
-		}
-		return *host, isTenant, *tenant, *tenantPassword
-	}
-
-	// if all user, password, tenant, and tenantPassword are initialized
-	// default to regular user
-	fmt.Println("Both tenant's password & username and user's password & username are defined")
-	fmt.Println("User's username and password are used for service initialization")
-	return *host, isTenant, *user, *password
-}
-
-func initializeGroupService(host string, isTenant bool, username, password string) (*service.NsGroupService) {
-	if isTenant {
-		groupService, err := service.NewNimbleGroupService(
-			service.WithHost(host),
-			service.WithTenantUser(username),
-			service.WithPassword(password))
-		if err != nil {
-			fmt.Printf("NewGroupService(): Unable to connect to group, err: %v", err.Error())
+func checkEnvironmentVariableExists(){
+	if (os.Getenv("SDK_TARGET_HOST") == "" ||
+		os.Getenv("SDK_TARGET_USER") == "" ||
+		os.Getenv("SDK_TARGET_USER_PASSWORD") == "") {
+			fmt.Println("ERROR: Missing one of these environment variables: SDK_TARGET_HOST, SDK_TARGET_USER, SDK_TARGET_USER_PASSWORD");
 			os.Exit(1)
 		}
-		return groupService
-	} else {
-		groupService, err := service.NewNimbleGroupService(
-			service.WithHost(host),
-			service.WithUser(username),
-			service.WithPassword(password))
-		if err != nil {
-			fmt.Printf("NewGroupService(): Unable to connect to group, err: %v", err.Error())
-			os.Exit(1)
-		}
-		return groupService
-	}
 }
 
 func main() {
-	host, isTenant, username, password := parseOptionalArgument()
+	checkEnvironmentVariableExists()
 
-	groupService := initializeGroupService(host, isTenant, username, password)
+	groupService, err := service.NewNimbleGroupService(
+		service.WithHost(os.Getenv("SDK_TARGET_HOST")),
+		service.WithUser(os.Getenv("SDK_TARGET_USER")),
+		service.WithPassword(os.Getenv("SDK_TARGET_USER_PASSWORD")))
+	if err != nil {
+		fmt.Printf("NewGroupService(): Unable to connect to group, err: %v", err.Error())
+		os.Exit(1)
+	}
 	defer groupService.LogoutService()
 
 	// set debug
